b074578c958f98816f0dd8f132757257712f814b
Alexandre <42672104+PROFeNoM@users.noreply.github.com>
2019-12-29 12:25:19 +0100

    Alternative

--- Python/3kyu/3kyu_The_Millionth_Fibonacci_Kata.py
+++ Python/3kyu/3kyu_The_Millionth_Fibonacci_Kata.py
@@ -17,3 +17,17 @@ def fib(n):
     if n<0:
         return fibs[k]
     return fibs[n]
+
+### An alternative
+
+def fib_alt(n):
+    if n == 0: return 0
+    k = abs(n)
+    v1, v2, v3 = 1, 1, 0    # initialise a matrix [[1,1],[1,0]]
+    for rec in bin(k)[3:]:  # perform fast exponentiation of the matrix (quickly raise it to the nth power)
+        calc = v2*v2
+        v1, v2, v3 = v1*v1+calc, (v1+v3)*v2, calc+v3*v3
+        if rec=='1':    v1, v2, v3 = v1+v2, v1, v2
+    if abs(n) % 2 == 0 and n < 0:
+        return -v2
+    return v2
