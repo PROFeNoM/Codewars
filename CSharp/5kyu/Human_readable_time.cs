be99e25faee6b18871c0aa60b13a4f9d4db389c0
Alexandre <42672104+PROFeNoM@users.noreply.github.com>
2020-02-08 16:00:42 +0100

    Create Human_readable_time.cs

---
+++ CSharp/5kyu/Human_readable_time.cs
@@ -0,0 +1,7 @@
+public static class TimeFormat
+{
+    public static string GetReadableTime(int seconds)
+    {
+        return string.Format("{0:d2}:{1:d2}:{2:d2}", seconds / 3600, seconds / 60 % 60, seconds % 60);
+    }
+}
