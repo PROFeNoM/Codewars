1fceb2aa21048b83f31af3ab8b1ebabc4727e033
Alexandre <42672104+PROFeNoM@users.noreply.github.com>
2020-08-18 18:50:41 +0200

    Delete Bouncing_balls.c

--- C/7kyu/Bouncing_balls.c
+++
@@ -1,8 +0,0 @@
-#include <math.h>
-
-int bouncingBall(double h, double b, double w) 
-{
-    if (h <= 0 || b <= 0 || b >= 1 || w >= h)
-        return -1;
-    return 2 * ceil( (log(w) - log(h)) / log(b) ) - 1;
-}
