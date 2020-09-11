6c0d42e36b4dd6c086e5acb2b5887c4669ec1dff
Alexandre <42672104+PROFeNoM@users.noreply.github.com>
2020-09-11 19:39:26 +0200

    Create Character_with_longest_consecutive_repetition.c

---
+++ C/6kyu/Character_with_longest_consecutive_repetition.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include <string.h>
+
+char longest_repetition(const char *chars, size_t *n) 
+{
+    char c = '\0';
+    int max = 0;
+    
+    while (*chars != '\0')
+    {
+        char current = *chars;
+        int count = 0;
+        while (*chars == current)
+        {
+            count++;
+            *chars++;
+        }
+        if (count > max)
+        {
+            c = current;
+            max = count;
+        }
+    }
+    
+    return *n = max, c;
+}
